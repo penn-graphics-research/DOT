4ab3baf210f843da4078dd237c60176e3a42fff4
Minchen Li <mincli@adobes-mbp-10.sea.adobe.com>
2018-07-11 13:37:50 -0700

    makePD for dynamic size matrices

--- FracCuts/Utils/IglUtils.hpp
+++ FracCuts/Utils/IglUtils.hpp
@@ -71,7 +71,8 @@ namespace FracCuts {
                 return;
             }
             Eigen::DiagonalMatrix<Scalar, size> D(eigenSolver.eigenvalues());
-            for(int i = 0; i < size; i++) {
+            int rows = ((size == Eigen::Dynamic) ? symMtr.rows() : size);
+            for(int i = 0; i < rows; i++) {
                 if(D.diagonal()[i] < 0.0) {
                     D.diagonal()[i] = 0.0;
                 }
