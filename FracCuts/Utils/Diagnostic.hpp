c10ddc694005f7ce6e2b94a837ed657027e626fd
Minchen Li <minchernl@gmail.com>
2018-09-22 15:29:27 -0400

    Added Tetgen test

--- FracCuts/Utils/Diagnostic.hpp
+++ FracCuts/Utils/Diagnostic.hpp
@@ -17,6 +17,7 @@
 #include <igl/boundary_loop.h>
 #include <igl/map_vertices_to_circle.h>
 #include <igl/harmonic.h>
+#include <igl/copyleft/tetgen/tetrahedralize.h>
 
 #include <cstdio>
 
@@ -742,6 +743,72 @@ namespace FracCuts{
                         
                         fclose(dirList);
                         
+                        break;
+                    }
+                        
+                    case 10: { // a tetrahedralization example
+                        // Input polygon
+                        Eigen::MatrixXd V;
+                        Eigen::MatrixXi F;
+                        Eigen::MatrixXd B;
+                        
+                        // Tetrahedralized interior
+                        Eigen::MatrixXd TV;
+                        Eigen::MatrixXi TT;
+                        Eigen::MatrixXi TF;
+                        
+                        using namespace Eigen;
+                        using namespace std;
+                        
+                        // Load a surface mesh
+                        igl::readOBJ(argv[3], V, F);
+                        
+                        // Tetrahedralize the interior
+                        igl::copyleft::tetgen::tetrahedralize(V,F,"pq1.414Y", TV,TT,TF);
+                        
+                        // Compute barycenters
+                        igl::barycenter(TV,TT,B);
+                        
+                        // Plot the generated mesh
+                        igl::opengl::glfw::Viewer viewer;
+                        viewer.callback_key_down = &key_down;
+                        
+                        unsigned char key = '5';
+                        if (key >= '1' && key <= '9')
+                        {
+                            double t = double((key - '1')+1) / 9.0;
+                            
+                            VectorXd v = B.col(2).array() - B.col(2).minCoeff();
+                            v /= v.col(0).maxCoeff();
+                            
+                            vector<int> s;
+                            
+                            for (unsigned i=0; i<v.size();++i)
+                                if (v(i) < t)
+                                    s.push_back(i);
+                            
+                            MatrixXd V_temp(s.size()*4,3);
+                            MatrixXi F_temp(s.size()*4,3);
+                            
+                            for (unsigned i=0; i<s.size();++i)
+                            {
+                                V_temp.row(i*4+0) = TV.row(TT(s[i],0));
+                                V_temp.row(i*4+1) = TV.row(TT(s[i],1));
+                                V_temp.row(i*4+2) = TV.row(TT(s[i],2));
+                                V_temp.row(i*4+3) = TV.row(TT(s[i],3));
+                                F_temp.row(i*4+0) << (i*4)+0, (i*4)+1, (i*4)+3;
+                                F_temp.row(i*4+1) << (i*4)+0, (i*4)+2, (i*4)+1;
+                                F_temp.row(i*4+2) << (i*4)+3, (i*4)+2, (i*4)+0;
+                                F_temp.row(i*4+3) << (i*4)+1, (i*4)+2, (i*4)+3;
+                            }
+                            
+                            viewer.data().clear();
+                            viewer.data().set_mesh(V_temp,F_temp);
+//                            viewer.data().set_mesh(TV,TF); // face inverted
+                            viewer.data().set_face_based(true);
+                        }
+                        
+                        viewer.launch();
                         break;
                     }
                         
